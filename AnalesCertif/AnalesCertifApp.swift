53848458dfbf55f30dc6180ee6718300eb5f3c48
Gilles Calma <archikeo30@gmail.com>
2024-07-29 14:57:09 +0200

    Initial Commit

---
+++ AnalesCertif/AnalesCertifApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalesCertifApp.swift
+//  AnalesCertif
+//
+//  Created by GILLES CALMA on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnalesCertifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
